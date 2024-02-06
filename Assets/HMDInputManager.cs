e8a9de09c552759e37fb672c9d6ece50c388efd6
Jaklina Velcheva <jaklina.velcheva@gmail.com>
2024-02-06 10:16:22 +0000

    first commit

---
+++ Assets/HMDInputManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInputManager : MonoBehaviour
+{
+    [SerializeField] GameObject VRRig;
+    [SerializeField] GameObject FPSRig;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        if (XRSettings.isDeviceActive || XRSettings.loadedDeviceName == "OpenXR Display")
+        {
+             Debug.Log("VR Rig Active Using device: "+ XRSettings.loadedDeviceName);
+             FPSRig.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("FPS Rig Active");
+            VRRig.SetActive(false);
+
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
